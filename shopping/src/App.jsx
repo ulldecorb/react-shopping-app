ff210bbffee6e520807882ebe4bcec131fbafc44
ulldecorb <ausias.bertran@gmail.com>
2022-05-02 15:41:01 +0200

    Add tailwind architecture

--- shopping/src/App.jsx
+++ shopping/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App__header">
-        <h1 className="header__app-title">SHOPPING TIME</h1>
+        <h1 className="text-3xl font-bold underline">SHOPPING TIME</h1>
         <h2>cart</h2>
       </header>
     </div>
---
+++ shopping/src/components/header/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <header className="header">
+      <h1 className="header__title">SHOP CENTER</h1>
+    </header>
+  );
+}
