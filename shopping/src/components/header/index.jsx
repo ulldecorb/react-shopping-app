f8a02b58148800b6c9cefa7139c4362ea3d567a8
ulldecorb <ausias.bertran@gmail.com>
2022-05-03 12:50:07 +0200

    Reinstall tailwindcss dependency

--- shopping/src/components/header/index.jsx
+++ shopping/src/components/header/index.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 export const Header = function Header() {
   return (
-    <header className="flex flex-row">
-      <h1 className="header__title">SHOP CENTER</h1>
-      <div className="header__box">
-        <div className="header__loggin">
+    <header className="flex flex-row justify-center basis-1/1">
+      <h1 className="text-3xl font-bold underline flex justify-center basis-1/2">SHOP CENTER</h1>
+      <div className="flex flex-row">
+        <div className="flex flex-row justify-center mr-4">
           LogIn or SignIn
         </div>
-        <div className="header__chart">
+        <div className="flex justify-center mr-4">
           chart
         </div>
       </div>
