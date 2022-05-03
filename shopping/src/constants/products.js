e599e533f83962033dde2820d88ab5991f99ad68
ulldecorb <ausias.bertran@gmail.com>
2022-05-03 13:01:40 +0200

    Expand products schema

--- shopping/src/constants/products.js
+++ shopping/src/constants/products.js
@@ -1,9 +1,16 @@
 export const Products = [
-  { id: '2', productName: 'product example', category: 'example' }
+  { id: '1', productName: 'circle', category: 'example' },
+  { id: '2', productName: 'square', category: 'example' },
+  { id: '3', productName: 'cat', category: 'example' },
+  { id: '4', productName: 'dog', category: 'example' },
+  { id: '5', productName: 'turtle', category: 'example' },
+  { id: '6', productName: 'flower', category: 'example' },
+  { id: '7', productName: 'rose', category: 'example' },
+  { id: '8', productName: 'user', category: 'example' }
 ];
 
 export const users = [
-  { user: 'john Doe', cart: [{ productId: 'xxx', quanty: '2' }], favorites: ['productId'] }
+  { userName: 'john Doe', cart: [{ productId: 'xxx', quanty: '2' }], favorites: ['productId'] }
 ];
 
 export default Products;
