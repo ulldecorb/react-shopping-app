a9256eea61844a3bef1b9e9d70667c67504e4629
ulldecorb <ausias.bertran@gmail.com>
2022-05-10 02:28:03 +0200

    Add quanty input

--- shopping/src/components/cart/index.jsx
+++ shopping/src/components/cart/index.jsx
@@ -3,8 +3,13 @@ import PropTypes from 'prop-types';
 import ProductCard from '../product-card';
 import Total from '../total';
 
-function Cart({ cart, handleToggleFavorites }) {
-  console.log(cart);
+function Cart({ cart, handleToggleFavorites, handleQuanty }) {
+  function getNumber(event) {
+    console.log(parseInt(event.target.value, 10));
+    const prop = { id: 'product.id', quanty: parseInt(event.target.value, 10) };
+    handleQuanty(prop);
+  }
+
   return (
     <main className="bg-yellow-600 pt-12 w-full h-screen flex flex-wrap justify-center content-start ">
       <h2 className="w-full text-center text-4xl text-lime-400 font-koulen flex flex-wrap justify-center content-start ">Cart</h2>
@@ -18,6 +23,16 @@ function Cart({ cart, handleToggleFavorites }) {
             {/* {user.cart[0].quanty} */}
             {product.quanty}
           </p>
+
+          <input
+            placeHolder="0"
+            type="number"
+            id="quantity"
+            name="quantity"
+            min="0"
+            max={product.stock}
+            onChange={getNumber}
+          />
         </article>
       ))}
       <Total cart={cart} />
@@ -27,7 +42,8 @@ function Cart({ cart, handleToggleFavorites }) {
 
 Cart.propTypes = {
   cart: PropTypes.arrayOf(PropTypes.object).isRequired,
-  handleToggleFavorites: PropTypes.func.isRequired
+  handleToggleFavorites: PropTypes.func.isRequired,
+  handleQuanty: PropTypes.func.isRequired
   // user: PropTypes.shape({
   //   userName: PropTypes.string.isRequired,
   //   userCart: PropTypes.arrayOf(PropTypes.object).isRequired,
